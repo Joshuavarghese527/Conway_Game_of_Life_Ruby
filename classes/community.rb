b10b622717323548f39f4c8c12c1752496dc17c0
Joshuavarghese527 <joshuavarghese527@gmail.com>
2018-01-22 17:45:03 -0500

    Implementation for number_of_neighbors method but needs refactor

--- classes/community.rb
+++ classes/community.rb
@@ -25,30 +25,97 @@ class Community
       if cell.location == Location::CENTER
         size += count_north_cell
         size += count_south_cell
+        size += count_east_cell
+        size += count_west_cell
+        size += count_northwest_cell
+        size += count_northeast_cell
+        size += count_southwest_cell
+        size += count_southeast_cell
       elsif cell.location == Location::NORTHWEST
         size += count_north_cell
         size += count_west_cell
-      end            
-      size
-    end            
+        size += count_center_cell
+      elsif cell.location == Location::NORTHEAST
+        size += count_north_cell
+        size += count_east_cell
+        size += count_center_cell
+      elsif cell.location == Location::SOUTHWEST
+        size += count_south_cell
+        size += count_west_cell
+        size += count_center_cell
+      elsif cell.location == Location::SOUTHEAST
+        size += count_south_cell                                          
+        size += count_east_cell
+        size += count_center_cell
+      elsif cell.location == Location::NORTH
+        size += count_northwest_cell
+        size += count_center_cell
+        size += count_northeast_cell
+        size += count_west_cell
+        size += count_east_cell
+      elsif cell.location == Location::SOUTH
+        size += count_southwest_cell
+        size += count_center_cell
+        size += count_southeast_cell
+        size += count_west_cell
+        size += count_east_cell
+      elsif cell.location == Location::EAST
+        size += count_northeast_cell
+        size += count_center_cell
+        size += count_southeast_cell
+        size += count_north_cell
+        size += count_south_cell
+      elsif cell.location == Location::WEST
+        size += count_northwest_cell                                          
+        size += count_center_cell
+        size += count_southwest_cell
+        size += count_north_cell
+        size += count_south_cell
+      end             
+    size
+  end            
 
- private            
+  private            
 
   def find_cell(location)
-    @grid_mapping.detect {|c| c.location == location}
-  end            
+    @grid.detect {|c| c.location == location}
+  end      
 
   def count_north_cell            
     count_cell(find_cell(Location::NORTH))
-  end            
+  end      
 
   def count_south_cell            
     count_cell(find_cell(Location::SOUTH))
-  end            
+  end     
+
+  def count_east_cell            
+    count_cell(find_cell(Location::EAST))
+  end    
 
   def count_west_cell            
     count_cell(find_cell(Location::WEST))
-  end            
+  end   
+
+  def count_northwest_cell            
+    count_cell(find_cell(Location::NORTHWEST))
+  end    
+
+  def count_southwest_cell            
+    count_cell(find_cell(Location::SOUTHWEST))
+  end   
+
+  def count_northeast_cell            
+    count_cell(find_cell(Location::NORTHEAST))
+  end    
+
+  def count_southeast_cell            
+    count_cell(find_cell(Location::SOUTHEAST))
+  end  
+
+  def count_center_cell              
+  count_cell(find_cell(Location::CENTER))
+  end   
 
   def count_cell(predicate)
     if predicate
--- test/game_of_life_test.rb
+++ test/game_of_life_test.rb
@@ -66,4 +66,39 @@ describe Community do
 
     assert_equal 2, community.number_of_neighbors_for(c1)
   end
+  it 'Number of neighbors is 8 for a center cell that has a neighbor in all locations' do            
+    c1 = Cell.new(Location::CENTER)
+    c2 = Cell.new(Location::NORTH)
+    c3 = Cell.new(Location::SOUTH)
+    c4 = Cell.new(Location::EAST)
+    c5 = Cell.new(Location::WEST)
+    c6 = Cell.new(Location::NORTHWEST)
+    c7 = Cell.new(Location::NORTHEAST)
+    c8 = Cell.new(Location::SOUTHWEST)
+    c9 = Cell.new(Location::SOUTHEAST)
+    community = Community.new
+    community.seed([c1, c2, c3, c4, c5, c6, c7, c8, c9])
+
+    assert_equal 8, community.number_of_neighbors_for(c1)
+  end
+  it 'Number of neighbors is 3 for a cell that has a neighbor in north, west, and center' do
+    c1 = Cell.new(Location::NORTHWEST)
+    c2 = Cell.new(Location::NORTH)
+    c3 = Cell.new(Location::WEST)
+    c4 = Cell.new(Location::CENTER)
+    community = Community.new
+    community.seed([c1, c2, c3, c4])
+
+    assert_equal 3, community.number_of_neighbors_for(c1)
+  end
+  it 'number of neighbors is 3 for a south west cell that has a neighbor in east, center and south' do            
+    c1 = Cell.new(Location::SOUTHWEST)
+    c2 = Cell.new(Location::SOUTH)
+    c3 = Cell.new(Location::WEST)
+    c4 = Cell.new(Location::CENTER)
+    community = Community.new            
+    community.seed([c1, c2, c3, c4])
+
+    assert_equal 3, community.number_of_neighbors_for(c1)
+  end  
 end  
